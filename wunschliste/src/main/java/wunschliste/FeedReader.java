f38b2c7e8a7436f7ebed1b41034cf51d9a0b6ec4
Martin Gutenbrunner <martingu1979@gmail.com>
2014-10-03 15:03:39 +0200

    splitting up stuff

--- wunschliste/src/main/java/wunschliste/FeedReader.java
+++ wunschliste/src/main/java/wunschliste/FeedReader.java
@@ -4,9 +4,9 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
+import java.util.Date;
 
+import com.rometools.rome.feed.synd.SyndEntry;
 import com.rometools.rome.feed.synd.SyndFeed;
 import com.rometools.rome.io.FeedException;
 import com.rometools.rome.io.SyndFeedInput;
@@ -14,8 +14,6 @@ import com.rometools.rome.io.XmlReader;
 
 public class FeedReader {
 
-  private static final DateFormat SDF = SimpleDateFormat.getDateTimeInstance();
-  
   public void readIt() {
     try {
       URI feedUri = new URI("http://www.wunschliste.de/xml/atom.pl?user_id=183326&key=093712692730865");
@@ -23,12 +21,20 @@ public class FeedReader {
       SyndFeedInput input = new SyndFeedInput();
       SyndFeed feed = input.build(new XmlReader(feedUri.toURL()));
   
-//      for (SyndEntry entry : feed.getEntries()) {
-//        Date updatedDate = entry.getUpdatedDate();
-//        System.out.println(SDF.format(updatedDate) + ": " + entry.getTitle());
-//      }
-      
-      System.out.println(feed);
+      for (SyndEntry entry : feed.getEntries()) {
+        String fullTitle = entry.getTitle();
+        int splitPos = fullTitle.lastIndexOf("-");
+        String titleEpisode = fullTitle.substring(0, splitPos);
+        String dateTimeChannel = fullTitle.substring(splitPos);
+        String channel = dateTimeChannel.substring(dateTimeChannel.lastIndexOf("/") + 2);
+        
+        splitPos = titleEpisode.lastIndexOf("(");
+        String titleSub = titleEpisode.substring(0, splitPos);
+        String episode = titleEpisode.substring(splitPos+1);
+        
+        Date updatedDate = entry.getUpdatedDate();
+        System.out.println(updatedDate + " | " + channel + " | " + titleSub + " | " + episode);
+      }
       
     } catch (URISyntaxException e) {
       e.printStackTrace();
